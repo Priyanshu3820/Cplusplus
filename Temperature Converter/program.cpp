2cbd8512a697dbd6fe58f8758a25d7b51816ad32
Kartik257354 <kartiksaxena2005@gmail.com>
2025-08-08 20:17:23 +0530

    Celsius to Kelvin and Fahrenheit to Kelvin and Vice Versa operations added

--- Temperature Converter/program.cpp
+++ Temperature Converter/program.cpp
@@ -1,27 +1,53 @@
 #include <iostream>
 using namespace std;
-#define a 9.0 / 5.0
-#define b 32.0
-float ctf(float c)
+#define a (9.0 / 5.0)
+#define b (32.0)
+void ctf(float c)
 {
     float f = a * c + b;
-    cout << c<<"C="<<f<<"F"<<endl<<endl;
+    cout << c << "C=" << f << "F" << endl
+         << endl;
 }
-float ftc(float f)
+void ftc(float f)
 {
-    float c = (f-b)/(a);
-    cout << f<<"F="<<c<<"C"<<endl<<endl;
+    float c = (f - b) / (a);
+    cout << f << "F=" << c << "C" << endl
+         << endl;
+}
+void ctk(float c)
+{
+    float k = c + 273;
+    cout << c << "C=" << k << "K" << endl
+         << endl;
+}
+void ktc(float k)
+{
+    float c = k - 273;
+    cout << k << "K=" << c << "C" << endl
+         << endl;
+}
+void ftk(float f)
+{
+    float k = 273 + (f - b) / (a);
+    cout << f << "F=" << k << "K" << endl
+         << endl;
+}
+void ktf(float k)
+{
+    float f = a * (k - 273) + b;
+    cout << k << "K=" << f << "F" << endl
+         << endl;
 }
 int main()
 {
-    while('a'<'b')
+    while (true)
     {
-    int n;
-    cout << "TEMPERATURE CONVERTER\n1. Celcius to fahrenheit\n2. Fahrenheit to celcius\n3. Exit\n";
-    cout << "Enter your choice(1-3): ";
-    cin >> n;
-    float c, f;
-    
+        int n;
+        cout << "TEMPERATURE CONVERTER\n1. Celcius to Fahrenheit\n2. Fahrenheit to Celcius\n3. Celcius to Kelvin\n4. Kelvin to Celsius\n5. Fahrenheit to Kelvin\n6. Kelvin to Fahrenheit\n7. Exit\n\n";
+        cout << "Enter your choice(1-7): ";
+        cin >> n;
+        float c, f, k;
+
         if (n == 1)
         {
             cout << "Enter temperature in celcius: ";
@@ -36,11 +62,37 @@ int main()
         }
         else if (n == 3)
         {
+            cout << "Enter temperature in celcius: ";
+            cin >> c;
+            ctk(c);
+        }
+        else if (n == 4)
+        {
+            cout << "Enter temperature in kelvin: ";
+            cin >> k;
+            ktc(k);
+        }
+        else if (n == 5)
+        {
+            cout << "Enter temperature in fahrenheit: ";
+            cin >> f;
+            ftk(f);
+        }
+        else if (n == 6)
+        {
+            cout << "Enter temperature in kelvin: ";
+            cin >> k;
+            ktf(k);
+        }
+        else if (n == 7)
+        {
             break;
         }
         else
         {
-            cout << "Invalid input"<<endl<<endl;
+            cout << "Invalid input" << endl
+                 << endl;
         }
     }
+    return 0;
 }
